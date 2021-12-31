1cdb13234cced892939b2eeab0aa23644a1a15ed
RexeR <96370706+rexerminator@users.noreply.github.com>
2021-12-31 21:00:25 +0330

    Update CircuitSimFrame.java

--- src/main/CircuitSimFrame.java
+++ src/main/CircuitSimFrame.java
@@ -7,7 +7,7 @@ public class CircuitSimFrame extends JFrame{
 	private static final long serialVersionUID = 1L;
 	//constructor ==================================================================================
 	public CircuitSimFrame() {
-		this.add(new CircuitSimPanelV2());
+		this.add(new CircuitSimPanel());
 		this.setTitle("Circuit simulator");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
